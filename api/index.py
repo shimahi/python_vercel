1e19eba0efadeb95c080d593d7fafa51d73fb86a
shimahi <shimahi@rear.jp>
2020-09-19 19:52:30 +0900

    graphql

--- api/index.py
+++ api/index.py
@@ -1,24 +1,36 @@
-from ariadne import QueryType, gql, make_executable_schema
 from ariadne.asgi import GraphQL
+from ariadne import (QueryType, load_schema_from_path,
+                     make_executable_schema)
 
-type_defs = gql("""
-    type Query {
-        hello: String!
-    }
-""")
 
-# Create type instance for Query type defined in our schema...
+type_defs = load_schema_from_path("typeDefs.graphql")
 query = QueryType()
-
-# ...and assign our resolver function to its "hello" field.
 
 
 @query.field("hello")
-def resolve_hello(_, info):
-    request = info.context["request"]
-    user_agent = request.headers.get("user-agent", "guest")
-    return "Hello, %s!" % user_agent
+def resolve_hello(*_, name):
+    return f"Hello, {name}!!"
+
+
+@query.field("goodbye")
+def resolve_goodbye(*_):
+    return "goodbye!"
+
+
+@query.field("members")
+def resolve_members(*_):
+    return [
+        {
+            "name": "shimahi",
+            "part": "guitar"
+        },
+        {
+            "name": "mochi",
+            "part": "vocal"
+        }
+    ]
 
 
 schema = make_executable_schema(type_defs, query)
+
 app = GraphQL(schema, debug=True)
